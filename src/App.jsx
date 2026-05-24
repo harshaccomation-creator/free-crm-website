4af4a5d75e16ebcc00d741d9deaa32e1529ae4d8
harshaccomation-creator <harsh.accomation@gmail.com>
2026-05-24 13:35:44 +0530

    Route login page to login URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import LandingPage from './pages/LandingPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
 
 export default function App() {
+  const path = window.location.pathname;
+  if (path === '/login') {
+    return <LoginPage />;
+  }
   return <LandingPage />;
 }
