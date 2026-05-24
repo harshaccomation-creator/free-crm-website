09a841fa5c200562ba2d9bc8fbca4249b21949fa
harshaccomation-creator <harsh.accomation@gmail.com>
2026-05-24 14:35:04 +0530

    Add super admin dashboard placeholder

---
+++ src/pages/dashboards/SuperAdminDashboard.jsx
@@ -0,0 +1,6 @@
+import '../../styles/dashboardBase.css';
+import '../../styles/superAdminDashboard.css';
+
+export default function SuperAdminDashboard() {
+  return <div className="sf-dashboard"><main className="sf-main"><h1>Super Admin Dashboard</h1></main></div>;
+}
