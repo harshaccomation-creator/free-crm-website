205370d93e61e92c6938b222beb05315a237037a
harshaccomation-creator <harsh.accomation@gmail.com>
2026-05-24 13:39:20 +0530

    Make App respond to route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import LandingPage from './pages/LandingPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 
 export default function App() {
-  const path = window.location.pathname;
+  const [path, setPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const syncPath = () => setPath(window.location.pathname);
+    window.addEventListener('popstate', syncPath);
+    window.addEventListener('salesflow:navigate', syncPath);
+    return () => {
+      window.removeEventListener('popstate', syncPath);
+      window.removeEventListener('salesflow:navigate', syncPath);
+    };
+  }, []);
+
   if (path === '/login') {
     return <LoginPage />;
   }
+
   return <LandingPage />;
 }
