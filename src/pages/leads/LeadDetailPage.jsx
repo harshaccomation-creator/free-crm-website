f6e36be251acc232590c4dca0faad6f20bae7b9d
harshaccomation-creator <harsh.accomation@gmail.com>
2026-05-25 14:00:38 +0530

    Import final premium activity section CSS

--- src/pages/leads/LeadDetailPage.jsx
+++ src/pages/leads/LeadDetailPage.jsx
@@ -2,6 +2,7 @@ import DashboardSidebar from '../../components/dashboard/DashboardSidebar.jsx';
 import { getLead } from './leadsData.js';
 import './LeadDetailImageExact.css';
 import './LeadDetailProfileExactFix.css';
+import './LeadDetailPremiumSection.css';
 
 function getCurrentRole() {
   const saved = window.localStorage.getItem('salesflowRole');
