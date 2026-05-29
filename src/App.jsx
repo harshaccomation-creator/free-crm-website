0827472329fa31fe74604de1063bcba43b6df2d4
harshaccomation-creator <harsh.accomation@gmail.com>
2026-05-29 17:56:10 +0530

    Import final logo force CSS

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,7 @@ import './styles/leadActivityTimelineFix.css';
 import './styles/superAdminCompactFix.css';
 import './styles/brandUnifiedCrmTheme.css';
 import './styles/salesflowLogoEverywhere.css';
+import './styles/zzLogoForce.css';
 
 const TAWK_WIDGET_ID = '6a185c426034501c34c0b3b0';
 const TAWK_PROPERTY_ID = '1jpnigpq8';
