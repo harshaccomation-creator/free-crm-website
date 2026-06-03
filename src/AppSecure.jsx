6c4fbf90bcda28dcc23027931b10eac0ee38de82
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-03 11:41:45 +0530

    Revert employee dashboard to previous V2 layout

--- src/AppSecure.jsx
+++ src/AppSecure.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import LandingPage from './pages/LandingPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
-import EmployeeDashboard from './pages/dashboards/EmployeeDashboardClean.jsx';
+import EmployeeDashboard from './pages/dashboards/EmployeeDashboardV2.jsx';
 import AdminDashboard from './pages/dashboards/AdminDashboard.jsx';
 import SuperAdminDashboard from './pages/dashboards/SuperAdminDashboard.jsx';
 import SuperAdminSectionPage from './pages/dashboards/SuperAdminSectionPage.jsx';
