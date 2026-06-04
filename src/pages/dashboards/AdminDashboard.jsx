fc3e3139077ac2acaf8eaf15e99fedc7ed099393
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-04 19:59:10 +0530

    Load company admin polish CSS from admin dashboard

--- src/pages/dashboards/AdminDashboard.jsx
+++ src/pages/dashboards/AdminDashboard.jsx
@@ -1,4 +1,5 @@
 import CompanyAdminPreview from '../../company-admin-new/CompanyAdminPreview.jsx';
+import '../../company-admin-new/companyAdminNewPolish.css';
 
 export default function AdminDashboard() {
   return <CompanyAdminPreview />;
