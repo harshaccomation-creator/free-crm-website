f4b78d0e61b54e97d17c1709282803032db244c2
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-05 16:06:23 +0530

    Restore build safe AppSecure

--- src/AppSecure.jsx
+++ src/AppSecure.jsx
@@ -5,4 +5,7 @@ import EmployeeDashboard from './pages/dashboards/EmployeeDashboardV2.jsx';
 import AdminDashboard from './pages/dashboards/AdminDashboard.jsx';
 import SuperAdminDashboard from './pages/dashboards/SuperAdminDashboard.jsx';
 import SuperAdminSectionPage from './pages/dashboards/SuperAdminSectionPage.jsx';
-import Lead
+import LeadListPage from './pages/leads/LeadListPage.jsx';
+import LeadDetailPage from './pages/leads/LeadDetailPage.jsx';
+import SettingsPage from './pages/shared/SettingsPage.jsx';
+import NotificationsPage from './pages/shared/NotificationsPage.jsx
