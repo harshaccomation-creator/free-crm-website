5417cb2836ac12c65990e49612ed4b99dc344cd8
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-05 15:54:46 +0530

    Restore compact AppSecure routing

--- src/AppSecure.jsx
+++ src/AppSecure.jsx
@@ -5,9 +5,4 @@ import EmployeeDashboard from './pages/dashboards/EmployeeDashboardV2.jsx';
 import AdminDashboard from './pages/dashboards/AdminDashboard.jsx';
 import SuperAdminDashboard from './pages/dashboards/SuperAdminDashboard.jsx';
 import SuperAdminSectionPage from './pages/dashboards/SuperAdminSectionPage.jsx';
-import LeadListPage from './pages/leads/LeadListPage.jsx';
-import LeadDetailPage from './pages/leads/LeadDetailPage.jsx';
-import EmployeeReportsPage from './pages/employee/EmployeeReportsPage.jsx';
-import PremiumProfilePage from './pages/employee/ProfilePagePremium.jsx';
-import EmployeeActivitiesPage from './pages/employee/EmployeeActivitiesPage.jsx';
-import EmployeeCalendarPage from
+import Lead
