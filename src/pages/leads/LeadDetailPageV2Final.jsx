f024f8f022a059020e0f3f0ce0e5063da478285f
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-05 11:51:26 +0530

    Add inline redesigned lead detail page

---
+++ src/pages/leads/LeadDetailPageV2Final.jsx
@@ -0,0 +1,5 @@
+import { useEffect, useMemo, useState } from 'react';
+import DashboardSidebar from '../../components/dashboard/DashboardSidebar.jsx';
+import { getLead as getMockLead } from './leadsData.js';
+import { getLead as getRealLead, isBackendConfigured, listActivities, listTasks } from '../../services/crmApi.js';
+import { Phone, Mail, MapPin, Calendar, MessageCircle, CheckSquare, Activity, Plus, Briefcase, User, Share2, Edit3, ArrowLeft } from 'lucide
