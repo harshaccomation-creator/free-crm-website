d8ec4af84a24970486f52ec8b343110ceaecfda6
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-05 22:24:08 +0530

    Repair app entry with safe router

--- src/AppSecure.jsx
+++ src/AppSecure.jsx
@@ -1,10 +1,6 @@
-import { useEffect, useState } from 'react';
 import LandingPage from './pages/LandingPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
-import EmployeeDashboard from './pages/dashboards/EmployeeDashboardV2.jsx';
-import AdminDashboard from './pages/dashboards/AdminDashboard.jsx';
-import LeadListPage from './pages/leads/LeadListPage.jsx';
-import LeadDetailPage from './pages/leads/LeadDetailPage.jsx';
 
-function cleanPath(path) {
-  return (path || '/').replace
+export default function AppSecure() {
+  return window.location.pathname === '/login' ? <LoginPage /> : <LandingPage />;
+}
