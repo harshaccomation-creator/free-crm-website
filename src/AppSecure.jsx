efcb60a7ad8a72994acbd202e504992a4755ca86
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-05 20:56:41 +0530

    Restore stable app router

--- src/AppSecure.jsx
+++ src/AppSecure.jsx
@@ -1,14 +1,8 @@
 import { useEffect, useState } from 'react';
 import LandingPage from './pages/LandingPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
+import EmployeeDashboard from './pages/dashboards/EmployeeDashboardV2.jsx';
 import AdminDashboard from './pages/dashboards/AdminDashboard.jsx';
-
-function cleanPath(value) {
-  return (value || '/').replace(/\/+$/, '') || '/';
-}
-
-export default function AppSecure() {
-  const [path, setPath] = useState(() => cleanPath(window.location.pathname));
-
-  useEffect(() => {
-    const
+import SuperAdminDashboard from './pages/dashboards/SuperAdminDashboard.jsx';
+import SuperAdminSectionPage from './pages/dashboards/SuperAdminSectionPage.jsx';
+import LeadListPage from './pages
