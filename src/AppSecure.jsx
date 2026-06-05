d9bb80aebb5aba4ece7dac7bef7e0beb87ae2a01
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-05 16:26:47 +0530

    Restore minimal valid AppSecure

--- src/AppSecure.jsx
+++ src/AppSecure.jsx
@@ -1,10 +1,5 @@
 import LandingPage from './pages/LandingPage.jsx';
-import LoginPage from './pages/LoginPage.jsx';
-import EmployeeDashboard from './pages/dashboards/EmployeeDashboardV2.jsx';
-import AdminDashboard from './pages/dashboards/AdminDashboard.jsx';
-import SuperAdminDashboard from './pages/dashboards/SuperAdminDashboard.jsx';
 
 export default function AppSecure() {
-  const path = window.location.pathname;
-
-  if (path === '/
+  return <LandingPage />;
+}
