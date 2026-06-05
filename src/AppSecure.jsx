d870e6c016f6bf673aff7967238571343f62249e
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-05 17:05:19 +0530

    Add login route

--- src/AppSecure.jsx
+++ src/AppSecure.jsx
@@ -1,12 +1,10 @@
 import LandingPage from './pages/LandingPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
 import AdminDashboard from './pages/dashboards/AdminDashboard.jsx';
 
 export default function AppSecure() {
   const path = window.location.pathname;
-
-  if (path.startsWith('/admin/dashboard')) {
-    return <AdminDashboard />;
-  }
-
+  if (path.indexOf('/admin/dashboard') === 0) return <AdminDashboard />;
+  if (path === '/login') return <LoginPage />;
   return <LandingPage />;
 }
