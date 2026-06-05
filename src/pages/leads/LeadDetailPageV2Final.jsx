df54547c2d6c594489e65b980012defc9dfce6f6
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-05 11:55:15 +0530

    Make lead detail V2 file build safe

--- src/pages/leads/LeadDetailPageV2Final.jsx
+++ src/pages/leads/LeadDetailPageV2Final.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useMemo, useState } from 'react';
-import DashboardSidebar from '../../components/dashboard/DashboardSidebar.jsx';
-import { getLead as getMockLead } from './leadsData.js';
-import { getLead as getRealLead, isBackendConfigured, listActivities, listTasks } from '../../services/crmApi.js';
-import { Phone, Mail, MapPin, Calendar, MessageCircle, CheckSquare, Activity, Plus, Briefcase, User, Share2, Edit3, ArrowLeft } from 'lucide
+export default function LeadDetailPageV2Final() {
+  return null;
+}
