3bdf7bf3349744fed6ff2a062f936180c6238435
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-05 17:29:05 +0530

    Fix client route re-render

--- src/AppSecure.jsx
+++ src/AppSecure.jsx
@@ -1,18 +1,14 @@
+import { useEffect, useState } from 'react';
 import LandingPage from './pages/LandingPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import AdminDashboard from './pages/dashboards/AdminDashboard.jsx';
 
-export default function AppSecure() {
-  const rawPath = window.location.pathname || '/';
-  const path = rawPath.replace(/\/+$/, '') || '/';
-
-  if (path.indexOf('/admin/dashboard') === 0) {
-    return <AdminDashboard />;
-  }
-
-  if (path === '/login' || path === '/signin' || path === '/auth/login') {
-    return <LoginPage />;
-  }
-
-  return <LandingPage />;
+function cleanPath(value) {
+  return (value || '/').replace(/\/+$/, '') || '/';
 }
+
+export default function AppSecure() {
+  const [path, setPath] = useState(() => cleanPath(window.location.pathname));
+
+  useEffect(() => {
+    const
