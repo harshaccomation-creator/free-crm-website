14c2928fc424da706c45f037b51d0701a2e44ea5
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-06 11:02:20 +0530

    Add employee shell component

---
+++ src/components/employee/EmployeeShell.jsx
@@ -0,0 +1,17 @@
+import './EmployeeShell.css';
+
+const menu = [
+  ['Dashboard','/employee/dashboard','▦'],
+  ['My Leads','/leads','♙'],
+  ['Contacts','/contacts','☎'],
+  ['Won','/employee/won','✓'],
+  ['Tasks','/employee/tasks','☑'],
+  ['Calendar','/employee/calendar','▣'],
+  ['Activities','/employee/activities','☷'],
+  ['Reports','/employee/reports','▥'],
+  ['Notifications','/notifications','♢'],
+  ['Profile','/employee/profile','♙'],
+  ['Settings','/settings','∘']
+];
+
+function go
