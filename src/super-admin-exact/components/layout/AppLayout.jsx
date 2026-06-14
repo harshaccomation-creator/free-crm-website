bbef8432380c88b90c63e0b0236a89524e9db8aa
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-14 23:08:40 +0530

    Match video app shell

--- src/super-admin-exact/components/layout/AppLayout.jsx
+++ src/super-admin-exact/components/layout/AppLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import Topbar from './Topbar';
 
 export default function AppLayout() {
   return (
-    <div className="flex h-screen overflow-hidden bg-gray-50">
+    <div className="min-h-screen bg-[#08111f] text-white">
       <Sidebar />
-      <main className="flex-1 ml-64 overflow-y-auto">
-        <Outlet />
-      </main>
+      <div className="ml-[330px] min-h-screen">
+        <Topbar />
+        <main className="p-9">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
