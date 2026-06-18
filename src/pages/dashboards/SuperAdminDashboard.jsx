8790bbc6e9e6736761d7e309784293accfe6f543
harshaccomation-creator <harsh.accomation@gmail.com>
2026-06-18 15:53:06 +0530

    Use uploaded Super Admin zip layout

--- src/pages/dashboards/SuperAdminDashboard.jsx
+++ src/pages/dashboards/SuperAdminDashboard.jsx
@@ -1,25 +1,5 @@
-import { useEffect, useState } from 'react';
-import SuperAdminOverviewPage from './SuperAdminOverviewPage.jsx';
-import SuperAdminSectionPage from './SuperAdminSectionPage.jsx';
-import '../../styles/superAdminReferenceTheme.css';
-
-function getCurrentView() {
-  return new URLSearchParams(window.location.search).get('view') || '';
-}
+import SalesFlowSuperAdminImported from '../super-admin/SalesFlowSuperAdminImported.jsx';
 
 export default function SuperAdminDashboard() {
-  const [view, setView] = useState(getCurrentView());
-
-  useEffect(() => {
-    const syncView = () => setView(getCurrentView());
-    window.addEventListener('popstate', syncView);
-    window.addEventListener('salesflow:navigate', syncView);
-    return () => {
-      window.removeEventListener('popstate', syncView);
-      window.removeEventListener('salesflow:navigate', syncView);
-    };
-  }, []);
-
-  if (view) return <SuperAdminSectionPage view={view} />;
-  return <SuperAdminOverviewPage />;
+  return <SalesFlowSuperAdminImported />;
 }
